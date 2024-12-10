33ee203e25a0fb864e874f370cafc46a2d91ab9b
Alyssa <Alyssagreychaos@gmail.com>
2024-12-09 23:57:20 -0500

    Customer Seating!
    Customers will now have a 50/50 chance to stay in the cafe for 3 - 8 seconds. They'll pick a random open seat, if none are open they'll just leave.

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -24,10 +25,13 @@ public class Customer : MonoBehaviour
     private List<Vector3Int> path;
     private int currentPathIndex = 0;
     public Coffee coffeOrder;
+    public bool StayingCustomer;
+    Vector3 originalExit;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        originalExit = new Vector3(exitTarget.transform.position.x, exitTarget.transform.position.y, 0);;
         spriteRenderer.sprite = customerSprites[Random.Range(0, customerSprites.Length)];
         GenerateOrder();
     }
@@ -45,10 +49,44 @@ public class Customer : MonoBehaviour
             if(enterTarget != null){
                 CustomerManager.updateCustomerQueueing(this);
             }
+            if(StayingCustomer){
+                sitDownAndWait();
+            }
             Pathfinding(exitTarget);
         }
     }
 
+    Vector3 seatSpot;
+    bool hasSeat = false;
+    void sitDownAndWait(){
+        if(!hasSeat){
+            seatSpot = CustomerManager.FindValidSeat();
+            if(seatSpot == new Vector3(0,0,0)){
+                StayingCustomer = false;
+                return;
+            }
+            hasSeat = true;
+        }
+        if(seatSpot != new Vector3(0,0,0)){
+            exitTarget.transform.position = seatSpot;
+            StartCoroutine(StartTimer());
+        }
+    }
+
+    bool isWaiting;
+    IEnumerator StartTimer()
+    {
+        if (isWaiting)
+            yield break;
+
+        isWaiting = true;
+
+        float randomTime = Random.Range(3f, 8f);
+        yield return new WaitForSeconds(randomTime);
+        CustomerManager.ReturnSeat(exitTarget.transform.position);
+        exitTarget.transform.position = originalExit;
+        StayingCustomer = false;
+    }
     void waitInLine(){
         Pathfinding(enterTarget);
     }
@@ -63,7 +101,7 @@ public class Customer : MonoBehaviour
     void Pathfinding(GameObject target){
         Vector3Int start = tilemap.WorldToCell(transform.position);
         Vector3Int goal = tilemap.WorldToCell(target.transform.position);
-        if(start == goal && goal == tilemap.WorldToCell(exitTarget.transform.position)){
+        if(start == goal && goal == tilemap.WorldToCell(exitTarget.transform.position) && !StayingCustomer){
             CustomerManager.destroyCustomer(this);
         }
         path = pathfinding.FindPath(start, goal);
--- Assets/Scripts/CustomerManager.cs
+++ Assets/Scripts/CustomerManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -13,17 +14,37 @@ public class CustomerManager : MonoBehaviour
 
     [SerializeField] Pathfinding pathFinding;
     [SerializeField] Tilemap tilemap;
+    [SerializeField] Tilemap midTilemap;
 
     List<Customer> customersWaiting = new();
     public static List<Customer> Customers = new();
+    List<Vector3> Seats = new();
 
     // How many customers are allowed to be spawned in at a time.
     public int customerCap = 1;
 
+    public Tile stoolTile;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        BoundsInt bounds = midTilemap.cellBounds;
+        for (int x = bounds.xMin; x < bounds.xMax; x++)
+        {
+            for (int y = bounds.yMin; y < bounds.yMax; y++)
+            {
+                Vector3Int position = new Vector3Int(x, y, 0);
+                TileBase tile = midTilemap.GetTile(position);
+                if(tile == null){
+                    continue;
+                }
+                if(tile == stoolTile){
+                    // Mid tile -3.5, .25 = Low tile -3.5, -.05
+                    Vector3 midLayerCords = midTilemap.CellToWorld(position);
+                    Seats.Add(new Vector3(midLayerCords.x, midLayerCords.y - .2f, 0));
+                }
+            }
+        }
     }
 
     // Update is called once per frame
@@ -37,13 +58,30 @@ public class CustomerManager : MonoBehaviour
         if(Customers.Count < customerCap){
             Customer newCustomer = Instantiate(defaultCustomer, customerSpawningPoint.transform.position, Quaternion.identity);
             newCustomer.enterTarget = Instantiate(customerWaitingPoint, waitingSpotPosition(customerWaitingPoint), Quaternion.identity);
-            newCustomer.exitTarget = customerLeavingPoint;
+            newCustomer.exitTarget = Instantiate(customerLeavingPoint, customerLeavingPoint.transform.position, Quaternion.identity);
             newCustomer.tilemap = tilemap;
             newCustomer.pathfinding = pathFinding;
             newCustomer.CustomerManager = this;
+            newCustomer.StayingCustomer = Random.Range(0, 2) == 0;
+            newCustomer.StayingCustomer = true;
             Customers.Add(newCustomer);
             customersWaiting.Add(newCustomer);
         }
+    }
+
+    public Vector3 FindValidSeat(){
+        if(Seats.Count == 0){
+            return new Vector3(0,0,0);
+        }
+        Debug.Log(Seats.Count);
+        int seatNumber = Random.Range(0, Seats.Count);
+        Vector3 temp = new Vector3(Seats[seatNumber].x, Seats[seatNumber].y, 0);
+        Seats.RemoveAt(seatNumber);
+        return temp;
+    }
+
+    public void ReturnSeat(Vector3 seat){
+        Seats.Add(seat);
     }
 
     Vector3 waitingSpotPosition(GameObject customerWaitingPoint){
