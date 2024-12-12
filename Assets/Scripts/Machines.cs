e125e35918f13d8b97e61aafae05a06b182553d7
Lyss <alyssagreychaos@gmail.com>
2024-12-12 14:52:31 -0500

    Fixed bug where player could use machine, without being next to it.

--- Assets/Scripts/Machines.cs
+++ Assets/Scripts/Machines.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Machines : MonoBehaviour
 {
     public bool trashCan;
     public float expenseCostPerUse = 1;
+    Tilemap lowTileMap;
+    Transform standingSpotObject;
+    Vector3Int standingSpotTile;
+    Vector3 standingSpot;
+    GameObject player;
+
+    void Start(){
+        lowTileMap = GameObject.Find("LowTileMap").GetComponent<Tilemap>();
+        standingSpotObject = transform.Find("Standing Spot");
+        standingSpotTile = lowTileMap.WorldToCell(standingSpotObject.position);
+        standingSpot = lowTileMap.CellToWorld(new Vector3Int(standingSpotTile.x, standingSpotTile.y, 0));
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     public static void ResetCoffee(){
         foreach (Coffee coffeeType in Coffee.CoffeeObjects){
@@ -15,7 +29,10 @@ public class Machines : MonoBehaviour
     }
 
     public void UseMachine(){
-        if(PlayerMovement.PlayerCoffee == null || this == trashCan){
+        if(!PlayerOnMachine()){
+            return;
+        }
+        if((PlayerMovement.PlayerCoffee == null || this == trashCan)){
             ResetCoffee();
         }
         foreach (Coffee coffeeType in Coffee.CoffeeObjects){
@@ -25,5 +42,13 @@ public class Machines : MonoBehaviour
                 return;
             }
         }
+    }
+
+    bool PlayerOnMachine(){
+        if(player.transform.position == standingSpot)
+        {
+            return true;
+        }
+        return false;
     }
 }
