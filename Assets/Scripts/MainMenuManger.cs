877f766bac45bafc6fa5afcdddef23a01a59b0c2
calebby58 <cbyates@protonmail.com>
2024-12-12 14:32:10 -0500

    EndOfDayRecap Changes
    slight changes to EndOfDayRecap scene. Added new "button" to display net profit after bills, calculated from dailyProfit - dailyBills (currently just set to 0f). Changed UI to show what each text slot does in editor.
    Began work on Main menu scene.
    Changed scenes in build profile. MainMenuScene is Scene 0, GameplayScene is scene 1. EndOfDayRecapScene is scene 2.

---
+++ Assets/Scripts/MainMenuManger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManger : MonoBehaviour
+{
+
+    void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void LoadGame()
+    {
+
+    }
+
+    void OptionsMenu()
+    {
+
+    }
+}
