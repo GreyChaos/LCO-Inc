1dd671d53dbc9d60a16f41a524b7709a497f12aa
calebby58 <cbyates@protonmail.com>
2024-12-15 18:10:58 -0500

    Created Background Music Manager Script
    Allows other scripts to change the background music currently playing. Plan to create seasons script tonight/tomorrow that will change between a couple songs based off of the month.

---
+++ Assets/Scripts/BGMusicManager.cs
@@ -0,0 +1,36 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BGMusicManager : MonoBehaviour
+{
+    // Makes a background music manager singleton. More or less a copy of the sound effect script, changed slightly so it continues looping.
+    public static BGMusicManager instance;
+
+    [SerializeField] private AudioSource BGMusicObject;
+
+    private void Start()
+    {
+        // Something to do with making this a singleton idk I watched a tutorial.
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Essentially copies sound effect script, except removes positioning based off input object and doesn't destroy the audioSource.
+    public void PlayAudioClip(AudioClip audioClip, float volume)
+    {
+        // If Background music object currently exists, destroy it.
+        if (BGMusicObject != null)
+            GameObject.Destroy(BGMusicObject);
+        
+        // Instantiate new background music object of input audo clip.
+        AudioSource audioSource = Instantiate(BGMusicObject, Vector3.zero, Quaternion.identity);
+
+        audioSource.clip = audioClip;
+
+        audioSource.volume = volume;
+
+        audioSource.Play();
+    }
+}
