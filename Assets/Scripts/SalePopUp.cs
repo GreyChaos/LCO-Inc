4a5b973c433032c5cad9e2f58ee3c55a97be1859
calebby58 <cbyates@protonmail.com>
2024-12-16 13:42:07 -0500

    Added Fade to SalePopUp Object
    SalesPopUp text fades out over the course of its lifetime. Note that it currently looks really bad but working on changing how it functions.
    Removed empty script from pause menu that caused error upon starting scene.

--- Assets/Scripts/SalePopUp.cs
+++ Assets/Scripts/SalePopUp.cs
@@ -1,13 +1,24 @@
 using TMPro;
+using System.Collections;
 using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
 
 public class SalePopUp : MonoBehaviour
 {
     // Float value for length of time popup remains on screen.
-    [SerializeField] float popUpLifetime = 1.5f;
+    [SerializeField] float popUpLifetime = 2f;
 
     // Initial velocity of the RigidBody2d behind the popups.
     [SerializeField] Vector2 initialVelocity;
+
+    public Color color;
+
+    private float currentAlpha = 1;
+    private float preFadeLifetime;
+    private float fadeLifetime;
+
+    // [SerializeField] TMP_Text popUpText;
 
     public Rigidbody2D rigidBody2d;
 
@@ -17,6 +28,27 @@ public class SalePopUp : MonoBehaviour
     {
         rigidBody2d.linearVelocity = initialVelocity;
         Destroy(gameObject, popUpLifetime);
+
+        preFadeLifetime = popUpLifetime * (2/3);
+        fadeLifetime = popUpLifetime - preFadeLifetime;
+        StartCoroutine("FadeText");
+    }
+
+    // Fades the pop up text over the course of its lifetime.
+    IEnumerator FadeText(   )
+    {
+        // Yields for 2/3 of the pop up lifetime before beginning fading the text.
+        yield return new WaitForSecondsRealtime(preFadeLifetime);
+
+        while(true)
+        {
+            // Increases the opacity in 10 steps over the remainder of the pop ups lifetime.
+            yield return new WaitForSeconds(fadeLifetime / 10);
+
+            currentAlpha /= 10;
+            color.a = currentAlpha;
+            gameObject.GetComponentInChildren<TMP_Text>().color = color;
+        }
     }
 
 }
