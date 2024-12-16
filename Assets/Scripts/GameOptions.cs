9f7026e5b2146fa675115070a5eb6f4389a26fbf
calebby58 <cbyates@protonmail.com>
2024-12-16 18:30:10 -0500

    Implemented Game Options PlayerPrefs
    The game now saves your options (currently just Music Volume and Effect Volume) between play sessions automatically!
    Don't ask me how I got it to work and don't breathe on it too hard any little movement might break it again.

--- Assets/Scripts/GameOptions.cs
+++ Assets/Scripts/GameOptions.cs
@@ -8,8 +8,8 @@ public class GameOptions : MonoBehaviour
     private float musicVolOption = 1f;
     private float effectVolOption = 1f;
 
-    public Slider mainMusicVolSlider;
-    public Slider mainEffectVolSlider;
+    public Slider musicVolSlider;
+    public Slider effectVolSlider;
 
     // Creates singelton instance and adds listener to detect audio volume slider changes.
     void Awake()
@@ -18,16 +18,37 @@ public class GameOptions : MonoBehaviour
         {
             Instance = this;
         }
+    }
 
+    void Start()
+    {
+        LoadOptions();
+        
         // Creates an event listener that, upon detecting a change in the slider value updates the music volume.
-        mainMusicVolSlider.onValueChanged.AddListener(value => {ChangeMusicVol(value); }); 
-        mainEffectVolSlider.onValueChanged.AddListener(value => {ChangeEffectVol(value); });
+        musicVolSlider.onValueChanged.AddListener(value => {ChangeMusicVol(value); }); 
+        effectVolSlider.onValueChanged.AddListener(value => {ChangeEffectVol(value); });
     }
 
     public void ChangeMusicVol(float musicVol)
     {
         musicVolOption = musicVol;
         BGMusicManager.Instance.ChangeBGMusicVolume();
+
+        SaveOptions();
+    }
+
+    public void ChangeEffectVol(float effectVol)
+    {
+        effectVolOption = effectVol;
+        SoundEffectManager.Instance.ChangeSoundEffectVolume();
+
+        SaveOptions();
+    }
+
+    public void UpdateSliderPosition()
+    {
+        musicVolSlider.value = musicVolOption;
+        effectVolSlider.value = effectVolOption;
     }
 
     public float GetMusicVol()
@@ -35,14 +56,20 @@ public class GameOptions : MonoBehaviour
         return musicVolOption;
     }
 
-    public void ChangeEffectVol(float effectVol)
-    {
-        effectVolOption = effectVol;
-        SoundEffectManager.Instance.ChangeSoundEffectVolume();
-    }
-
     public float GetEffectVol()
     {
         return effectVolOption;
+    }
+
+    public void SaveOptions()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", musicVolOption);
+        PlayerPrefs.SetFloat("EffectsVolume", effectVolOption);
+    }
+
+    public void LoadOptions()
+    {
+        musicVolOption = PlayerPrefs.GetFloat("MusicVolume");
+        effectVolOption = PlayerPrefs.GetFloat("effectVolOption");
     }
 }
